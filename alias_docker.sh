ce40a313f04ac5a267875f6a4e1623cbf3f340fc
Paul Moore <williampaulmoore@gmail.com>
2023-07-09 10:25:43 +0100

    chore: Scripts that define standard alias for git and docker

---
+++ alias_docker.sh
@@ -0,0 +1,17 @@
+# container
+# network
+# volume
+# image
+alias d="docker"
+alias dk_alias="alias | sed 's/^alias //' | grep dk"
+alias dkcl="docker ps"
+alias dkci="docker inspect"
+alias dkcr="docker rm"
+alias dknl="docker network ls"
+alias dkni="docker network inspect"
+alias dkvl="docker volume ls"
+alias dkvi="docker volume inspect"
+alias dkil="docker image ls"
+alias dkii="docker image inspect"
+alias dkir="docker image rm"
+
